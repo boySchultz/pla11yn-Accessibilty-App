834acd0b5015c037bee0cc5d84dd69bebfe68753
Max Weinmann <max.weinmann@uxma.com>
2023-05-12 17:39:49 +0200

    TEXT PRESENTATION ally methods.3:paragraph height AAA.2

--- src/feats/browser/Browser.tsx
+++ src/feats/browser/Browser.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useRef } from "react";
 import { WebView, WebViewMessageEvent } from "react-native-webview";
 import {
-	TouchableOpacity,
-	Animated,
-	PanResponder,
-	StyleSheet,
-	View,
+  TouchableOpacity,
+  Animated,
+  PanResponder,
+  StyleSheet,
+  View,
 } from "react-native";
 import { Appbar, Button } from "react-native-paper";
 import { SearchBar } from "./SearchBar";
@@ -15,126 +15,127 @@ import { SettingsState } from "../../store/StoreTypes";
 import { useAllyStore } from "../../store/allyStore";
 
 const Browser = () => {
-	const { writeSetting } = useAllyStore();
-	const [url, setUrl] = useState("https://www.sv-kampen.de/");
-	const [showSettings, setShowSettings] = useState(false);
-	const webViewRef = useRef<WebView | null>(null);
+  const { writeSetting } = useAllyStore();
+  const [url, setUrl] = useState("https://www.sv-kampen.de/");
+  const [showSettings, setShowSettings] = useState(false);
+  const webViewRef = useRef<WebView | null>(null);
 
-	//region drawer animation
-	const bottomDrawerAnim = useRef(new Animated.Value(0)).current;
-	const panResponder = useRef(
-		PanResponder.create({
-			onMoveShouldSetPanResponder: (evt, gestureState) => {
-				// Only allow dragging up from the bottom
-				return gestureState.dy < 0;
-			},
-			onPanResponderMove: (evt, gestureState) => {
-				// Update the bottomDrawerAnim based on the drag distance
-				bottomDrawerAnim.setValue(-gestureState.dy);
-			},
-			onPanResponderRelease: (evt, gestureState) => {
-				// If the drawer was not dragged more than 200px, open it
-				if (gestureState.dy > -200) {
-					Animated.spring(bottomDrawerAnim, {
-						toValue: 0,
-						useNativeDriver: false,
-					}).start();
-				} else {
-					// If the drawer was dragged more than 200px, close it
-					closeDrawer();
-				}
-			},
-		})
-	).current;
+  //region drawer animation
+  const bottomDrawerAnim = useRef(new Animated.Value(0)).current;
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: (evt, gestureState) => {
+        // Only allow dragging up from the bottom
+        return gestureState.dy < 0;
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        // Update the bottomDrawerAnim based on the drag distance
+        bottomDrawerAnim.setValue(-gestureState.dy);
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        // If the drawer was not dragged more than 200px, open it
+        if (gestureState.dy > -200) {
+          Animated.spring(bottomDrawerAnim, {
+            toValue: 0,
+            useNativeDriver: false,
+          }).start();
+        } else {
+          // If the drawer was dragged more than 200px, close it
+          closeDrawer();
+        }
+      },
+    })
+  ).current;
 
-	const openDrawer = () => {
-		Animated.timing(bottomDrawerAnim, {
-			toValue: 1,
-			duration: 500,
-			useNativeDriver: false,
-		}).start();
-		setShowSettings(true);
-	};
+  const openDrawer = () => {
+    Animated.timing(bottomDrawerAnim, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    setShowSettings(true);
+  };
 
-	const closeDrawer = () => {
-		Animated.timing(bottomDrawerAnim, {
-			toValue: 0,
-			duration: 500,
-			useNativeDriver: false,
-		}).start(() => setShowSettings(false));
-	};
-	//endregion
+  const closeDrawer = () => {
+    Animated.timing(bottomDrawerAnim, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: false,
+    }).start(() => setShowSettings(false));
+  };
+  //endregion
 
-	//set initial value for settings
-	const handleMessage = (event: WebViewMessageEvent) => {
-		const eventState = JSON.parse(
-			event.nativeEvent.data
-		) as Partial<SettingsState>;
-		console.log(
-			`setting initial value of ${eventState.settingsKey} to ${eventState.initialValue}`
-		);
-		writeSetting({
-			initialValue: eventState.initialValue,
-			settingsKey: eventState.settingsKey,
-		});
-	};
+  //set initial value for settings
+  const handleMessage = (event: WebViewMessageEvent) => {
+    const eventState = JSON.parse(
+      event.nativeEvent.data
+    ) as Partial<SettingsState>;
+    console.log(
+      eventState,
+      `setting initial value of ${eventState.settingsKey} to ${eventState.initialValue}`
+    );
+    writeSetting({
+      initialValue: eventState.initialValue,
+      settingsKey: eventState.settingsKey,
+    });
+  };
 
-	return (
-		<>
-			{/*Searchbar*/}
-			<Appbar.Header>
-				<TouchableOpacity onPress={showSettings ? closeDrawer : openDrawer}>
-					<Appbar.Action icon="menu"/>
-				</TouchableOpacity>
-				<SearchBar setUrl={setUrl} url={url}/>
-			</Appbar.Header>
+  return (
+    <>
+      {/*Searchbar*/}
+      <Appbar.Header>
+        <TouchableOpacity onPress={showSettings ? closeDrawer : openDrawer}>
+          <Appbar.Action icon="menu" />
+        </TouchableOpacity>
+        <SearchBar setUrl={setUrl} url={url} />
+      </Appbar.Header>
 
-			{/*WebView*/}
-			<WebView
-				ref={webViewRef}
-				source={{ uri: url }}
-				onMessage={(event) => handleMessage(event)}
-			/>
+      {/*WebView*/}
+      <WebView
+        ref={webViewRef}
+        source={{ uri: url }}
+        onMessage={(event) => handleMessage(event)}
+      />
 
-			{/*Settings*/}
-			{showSettings && (
-				<Animated.View
-					style={[
-						styles.drawer,
-						{
-							transform: [
-								{
-									translateY: bottomDrawerAnim.interpolate({
-										inputRange: [0, 1],
-										outputRange: [200, 0],
-										//clamp: if the input value goes beyond the specified inputRange, the output value will be clamped to the nearest output range value.
-										extrapolate: "clamp",
-									}),
-								},
-							],
-						},
-					]}
-					{...panResponder.panHandlers}
-				>
-					<AllySettings webViewRef={webViewRef}/>
-					<View style={{ padding: 16 }}>
-						<Button onPress={closeDrawer}>Close Menu</Button>
-					</View>
-				</Animated.View>
-			)}
-		</>
-	);
+      {/*Settings*/}
+      {showSettings && (
+        <Animated.View
+          style={[
+            styles.drawer,
+            {
+              transform: [
+                {
+                  translateY: bottomDrawerAnim.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [200, 0],
+                    //clamp: if the input value goes beyond the specified inputRange, the output value will be clamped to the nearest output range value.
+                    extrapolate: "clamp",
+                  }),
+                },
+              ],
+            },
+          ]}
+          {...panResponder.panHandlers}
+        >
+          <AllySettings webViewRef={webViewRef} />
+          <View style={{ padding: 16 }}>
+            <Button onPress={closeDrawer}>Close Menu</Button>
+          </View>
+        </Animated.View>
+      )}
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-	drawer: {
-		color: theme.colors.primary,
-		backgroundColor: theme.colors.secondary,
-		position: "absolute",
-		bottom: 0,
-		left: 0,
-		right: 0,
-		height: 400,
-	},
+  drawer: {
+    color: theme.colors.primary,
+    backgroundColor: theme.colors.secondary,
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 400,
+  },
 });
 export default Browser;
--- src/feats/settings/settingsConfig.ts
+++ src/feats/settings/settingsConfig.ts
@@ -1,69 +1,70 @@
 import {
-	setWordSpacing,
-	setLineHeight,
-	setParagraphHeight,
-	setLetterSpacing,
-	setFontSize, setTextAlignment
-} from './methods/allyMethods';
-import React from 'react';
-import { WebView } from 'react-native-webview';
-import { SettingsState } from '../../store/StoreTypes';
+  setWordSpacing,
+  setLineHeight,
+  setParagraphHeight,
+  setLetterSpacing,
+  setFontSize,
+  setTextAlignment,
+} from "./methods/allyMethods";
+import React from "react";
+import { WebView } from "react-native-webview";
+import { SettingsState } from "../../store/StoreTypes";
 
 export type SettingsKey =
-	'setWordSpacing'
-	| 'setFontSize'
-	| 'setLineHeight'
-	| 'setLetterSpacing'
-	| 'setParagraphHeight'
-	| 'setTextAlignment';
+  | "setWordSpacing"
+  | "setFontSize"
+  | "setLineHeight"
+  | "setLetterSpacing"
+  | "setParagraphHeight"
+  | "setTextAlignment";
 
 export interface AllySetting {
-	settingsKey: SettingsKey;
-	onPress: (
-		ref: React.MutableRefObject<WebView | null>,
-		getSettingsState: (
-			searchSetting: Partial<SettingsState>
-		) => SettingsState | undefined
-	) => void;
-	title: string;
-	steps: number;
+  settingsKey: SettingsKey;
+  onPress: (
+    ref: React.MutableRefObject<WebView | null>,
+    getSettingsState: (
+      searchSetting: Partial<SettingsState>
+    ) => SettingsState | undefined
+  ) => void;
+  title: string;
+  steps: number;
 }
 
 export const settingsConfig: AllySetting[] = [
-	{
-		settingsKey: 'setTextAlignment',
-		onPress: setTextAlignment,
-		title: 'Text Alignment',
-		steps: 3,
-	},
-	{
-		settingsKey: 'setFontSize',
-		onPress: setFontSize,
-		title: 'Font Size',
-		steps: 3,
-	},
-	{
-		settingsKey: 'setLineHeight',
-		onPress: setLineHeight,
-		title: 'Line Height',
-		steps: 2,
-	},
-	{
-		settingsKey: 'setParagraphHeight',
-		onPress: setParagraphHeight,
-		title: 'Paragraph Height',
-		steps: 2,
-	},
-	{
-		settingsKey: 'setLetterSpacing',
-		onPress: setLetterSpacing,
-		title: 'Letter Spacing',
-		steps: 3,
-	},
-	{
-		settingsKey: 'setWordSpacing',
-		onPress: setWordSpacing,
-		title: 'Word Spacing',
-		steps: 3,
-	},
+  {
+    settingsKey: "setTextAlignment",
+    onPress: setTextAlignment,
+    title: "Text Alignment",
+    steps: 3,
+  },
+  {
+    settingsKey: "setFontSize",
+    onPress: setFontSize,
+    title: "Font Size",
+    steps: 3,
+  },
+  {
+    settingsKey: "setLineHeight",
+    onPress: setLineHeight,
+    title: "Line Height",
+    steps: 2,
+  },
+  {
+    settingsKey: "setParagraphHeight",
+    onPress: setParagraphHeight,
+    title: "Paragraph Height",
+    steps: 2,
+  },
+  {
+    settingsKey: "setLetterSpacing",
+    onPress: setLetterSpacing,
+    title: "Letter Spacing",
+    steps: 3,
+  },
+  {
+    settingsKey: "setWordSpacing",
+    onPress: setWordSpacing,
+    title: "Word Spacing",
+    steps: 3,
+  },
 ];
