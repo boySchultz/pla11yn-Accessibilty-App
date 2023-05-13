c4d3b2437577c95a2ae05292efd47d53e4397dab
Max Weinmann <max.weinmann@uxma.com>
2023-05-13 15:43:46 +0200

    layout

--- src/feats/browser/Browser.tsx
+++ src/feats/browser/Browser.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 import { WebView, WebViewMessageEvent } from "react-native-webview";
-import { TouchableOpacity, Animated, StyleSheet, View, } from "react-native";
+import { TouchableOpacity, Animated, StyleSheet, View } from "react-native";
 import { Appbar, Button } from "react-native-paper";
 import { SearchBar } from "./SearchBar";
 import { AllySettings } from "../settings/AllySettings";
@@ -9,98 +9,98 @@ import { SettingsState } from "../../store/StoreTypes";
 import { useAllyStore } from "../../store/allyStore";
 
 const Browser = () => {
-	const { writeSetting } = useAllyStore();
-	const [url, setUrl] = useState("https://www.sv-kampen.de/");
-	const [showSettings, setShowSettings] = useState(false);
-	const webViewRef = useRef<WebView | null>(null);
+  const { writeSetting } = useAllyStore();
+  const [url, setUrl] = useState("https://www.sv-kampen.de/");
+  const [showSettings, setShowSettings] = useState(false);
+  const webViewRef = useRef<WebView | null>(null);
 
-	//region drawer animation
-	const bottomDrawerAnim = useRef(new Animated.Value(0)).current;
-	const openDrawer = () => {
-		Animated.timing(bottomDrawerAnim, {
-			toValue: 1,
-			duration: 500,
-			useNativeDriver: false,
-		}).start();
-		setShowSettings(true);
-	};
+  //region drawer animation
+  const bottomDrawerAnim = useRef(new Animated.Value(0)).current;
+  const openDrawer = () => {
+    Animated.timing(bottomDrawerAnim, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    setShowSettings(true);
+  };
 
-	const closeDrawer = () => {
-		Animated.timing(bottomDrawerAnim, {
-			toValue: 0,
-			duration: 500,
-			useNativeDriver: true,
-		}).start(() => setShowSettings(false));
-	};
-	//endregion
+  const closeDrawer = () => {
+    Animated.timing(bottomDrawerAnim, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start(() => setShowSettings(false));
+  };
+  //endregion
 
-	//set initial value for settings
-	const handleMessage = (event: WebViewMessageEvent) => {
-		const eventState = JSON.parse(
-			event.nativeEvent.data
-		) as Partial<SettingsState>;
-		console.log(
-			eventState,
-			`setting initial value of ${eventState.settingsKey} to ${eventState.initialValue}`
-		);
-		writeSetting({
-			initialValue: eventState.initialValue,
-			settingsKey: eventState.settingsKey,
-		});
-	};
+  //set initial value for settings
+  const handleMessage = (event: WebViewMessageEvent) => {
+    const eventState = JSON.parse(
+      event.nativeEvent.data
+    ) as Partial<SettingsState>;
+    console.log(
+      eventState,
+      `setting initial value of ${eventState.settingsKey} to ${eventState.initialValue}`
+    );
+    writeSetting({
+      initialValue: eventState.initialValue,
+      settingsKey: eventState.settingsKey,
+    });
+  };
 
-	return (
-		<>
-			{/*Searchbar*/}
-			<Appbar.Header>
-				<TouchableOpacity onPress={showSettings ? closeDrawer : openDrawer}>
-					<Appbar.Action icon="menu"/>
-				</TouchableOpacity>
-				<SearchBar setUrl={setUrl} url={url}/>
-			</Appbar.Header>
+  return (
+    <>
+      {/*Searchbar*/}
+      <Appbar.Header>
+        <TouchableOpacity onPress={showSettings ? closeDrawer : openDrawer}>
+          <Appbar.Action icon="menu" />
+        </TouchableOpacity>
+        <SearchBar setUrl={setUrl} url={url} />
+      </Appbar.Header>
 
-			{/*WebView*/}
-			<WebView
-				ref={webViewRef}
-				source={{ uri: url }}
-				onMessage={(event) => handleMessage(event)}
-			/>
-			{/*Settings*/}
-			{showSettings && (
-				<Animated.View
-					style={[
-						styles.drawer,
-						{
-							transform: [
-								{
-									translateY: bottomDrawerAnim.interpolate({
-										inputRange: [0, 1],
-										outputRange: [500, 0],
-									}),
-								},
-							],
-						},
-					]}
-				>
-					<AllySettings webViewRef={webViewRef}/>
-					<View style={{ padding: 16 }}>
-						<Button onPress={closeDrawer}>Close Menu</Button>
-					</View>
-				</Animated.View>
-			)}
-		</>
-	);
+      {/*WebView*/}
+      <WebView
+        ref={webViewRef}
+        source={{ uri: url }}
+        onMessage={(event) => handleMessage(event)}
+      />
+      {/*Settings*/}
+      {showSettings && (
+        <Animated.View
+          style={[
+            styles.drawer,
+            {
+              transform: [
+                {
+                  translateY: bottomDrawerAnim.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [500, 0],
+                  }),
+                },
+              ],
+            },
+          ]}
+        >
+          <AllySettings webViewRef={webViewRef} />
+          <View style={{ padding: 16 }}>
+            <Button onPress={closeDrawer}>Close Menu</Button>
+          </View>
+        </Animated.View>
+      )}
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-	drawer: {
-		color: theme.colors.primary,
-		backgroundColor: theme.colors.secondary,
-		position: "absolute",
-		bottom: 0,
-		left: 0,
-		right: 0,
-		height: 500,
-	},
+  drawer: {
+    color: theme.colors.primary,
+    backgroundColor: theme.colors.secondary,
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 500,
+  },
 });
 export default Browser;
