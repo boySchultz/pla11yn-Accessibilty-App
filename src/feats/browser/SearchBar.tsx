af4a6f59fb27b5b289d1e7cd85135de3b3bb9fb6
Max Weinmann <max.weinmann@uxma.com>
2023-05-16 14:07:32 +0200

    style updates

--- src/feats/browser/SearchBar.tsx
+++ src/feats/browser/SearchBar.tsx
@@ -5,45 +5,50 @@ import theme from "../../../theme";
 import { handleSearch } from "./utils/urlHelpers";
 
 interface SearchBarProps {
-  url: string;
-  setUrl: (url: string) => void;
+	url: string;
+	setUrl: (url: string) => void;
 }
 
 export const SearchBar = ({ setUrl, url }: SearchBarProps) => {
-  const [inputText, setInputText] = useState(url);
+	const [inputText, setInputText] = useState(url);
 
-  useEffect(() => {
-    setInputText(url);
-  }, [url]);
+	useEffect(() => {
+		setInputText(url);
+	}, [url]);
 
-  return (
-    <>
-      <TextInput
-        placeholder="Search or enter URL"
-        textContentType={"URL"}
-        style={styles.input}
-        value={inputText}
-        onChangeText={(change) => setInputText(change)}
-        numberOfLines={1}
-      />
-      <Button
-        mode={"text"}
-        buttonColor={theme.colors.secondary}
-        onPress={() => handleSearch(inputText, setUrl)}
-      >
-        Search
-      </Button>
-    </>
-  );
+	return (
+		<>
+			<TextInput
+				placeholder="Search or enter URL"
+				textContentType={"URL"}
+				style={styles.input}
+				value={inputText}
+				onChangeText={(change) => setInputText(change)}
+				numberOfLines={1}
+				role="searchbox"
+				aria-label="Search or enter URL"
+			/>
+			<Button
+				mode={"text"}
+				buttonColor={theme.colors.secondary}
+				onPress={() => handleSearch(inputText, setUrl)}
+				role="button"
+				aria-label="Search"
+			>
+				Search
+			</Button>
+		</>
+	);
 };
 
 const styles = StyleSheet.create({
-  input: {
-    flex: 1,
-    overflow: "hidden",
-    backgroundColor: theme.colors.background,
-    height: 40,
-    marginHorizontal: 10,
-    paddingHorizontal: 10,
-  },
+	input: {
+		fontSize: 18,
+		flex: 1,
+		overflow: "hidden",
+		backgroundColor: theme.colors.background,
+		height: 40,
+		marginHorizontal: 12,
+		paddingHorizontal: 12,
+	},
 });
