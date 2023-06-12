ed2895e25aa8615c3c6b97db3fedf7bc29506f69
Max Weinmann <max.weinmann@uxma.com>
2023-06-12 23:32:46 +0200

    even more lay out changes

--- src/feats/settings/AllySettings.tsx
+++ src/feats/settings/AllySettings.tsx
@@ -7,91 +7,90 @@ import theme from "../../../theme";
 import { IconButton } from "react-native-paper";
 
 interface AllySettingsProps {
-	webViewRef: MutableRefObject<WebView | null>;
-	settingsEnabled: boolean;
+  webViewRef: MutableRefObject<WebView | null>;
+  settingsEnabled: boolean;
 }
 
 interface CollapsibleWrapperProps {
-	title: string;
-	children: React.ReactNode;
+  title: string;
+  children: React.ReactNode;
 }
 
 export const AllySettings = ({
-	                             webViewRef,
-	                             settingsEnabled,
-                             }: AllySettingsProps) => {
+  webViewRef,
+  settingsEnabled,
+}: AllySettingsProps) => {
+  const CollapsibleWrapper = ({ children, title }: CollapsibleWrapperProps) => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
 
-	const CollapsibleWrapper = ({ children, title }: CollapsibleWrapperProps) => {
-		const [isCollapsed, setIsCollapsed] = useState(false);
+    const toggleCollapse = () => {
+      setIsCollapsed((prevCollapsed) => !prevCollapsed);
+    };
 
-		const toggleCollapse = () => {
-			setIsCollapsed((prevCollapsed) => !prevCollapsed);
-		};
+    return (
+      <>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <IconButton
+            icon={isCollapsed ? "chevron-down" : "chevron-up"}
+            size={40}
+            onPress={toggleCollapse}
+          />
+          <Text style={theme.ally.text}>{title}</Text>
+        </View>
+        {!isCollapsed && children}
+      </>
+    );
+  };
 
-		return (
-			<>
-			<View style={{
-				flexDirection: "row",
-				// justifyContent: "space-between",
-				alignItems: "center",
-			}}>
-				<IconButton
-					icon={isCollapsed ? 'chevron-down' : 'chevron-up'}
-					size={40}
-					onPress={toggleCollapse}
-				/>
-				<Text style={theme.ally.text}>{title}</Text>
-			</View>
-				{!isCollapsed && children}
-			</>
+  const renderSettings = ({ item }: any) => {
+    return (
+      <View style={styles.tileContainer}>
+        <SettingsTile
+          settingsEnabled={settingsEnabled}
+          webViewRef={webViewRef}
+          settingsKey={item.settingsKey}
+          allyMethod={item.allyMethod}
+          title={item.title}
+          steps={item.steps}
+        />
+      </View>
+    );
+  };
 
-		);
-	};
-
-	const renderSettings = ({ item }: any) => {
-		return (
-			<View style={styles.tileContainer}>
-				<SettingsTile
-					settingsEnabled={settingsEnabled}
-					webViewRef={webViewRef}
-					settingsKey={item.settingsKey}
-					allyMethod={item.allyMethod}
-					title={item.title}
-					steps={item.steps}
-				/>
-			</View>
-		);
-	};
-
-	return (
-		<View style={styles.container}>
-			<CollapsibleWrapper title={'Visual Representation'}>
-				<FlatList
-					data={settingsConfig}
-					numColumns={2}
-					renderItem={renderSettings}
-					columnWrapperStyle={styles.row} // Apply styles to each row
-				/>
-			</CollapsibleWrapper>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <CollapsibleWrapper title={"Visual Representation"}>
+        <FlatList
+          data={settingsConfig}
+          numColumns={2}
+          renderItem={renderSettings}
+          columnWrapperStyle={styles.row} // Apply styles to each row
+        />
+      </CollapsibleWrapper>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: theme.colors.background,
-		flex: 1,
-		paddingHorizontal: 16,
-		paddingVertical: 8,
-	},
-	row: {
-		flex: 1,
-		justifyContent: "space-between",
-	},
-	tileContainer: {
-		flex: 1,
-		margin: 8,
-		minWidth: 96, // Set minimum width of each item to 96 for Target Size (Level AAA)
-		minHeight: 96, // Set minimum width of each item to 96 for Target Size (Level AAA)
-	},
+  container: {
+    backgroundColor: theme.colors.background,
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  row: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  tileContainer: {
+    flex: 1,
+    margin: 8,
+    minWidth: 96, // Set minimum width of each item to 96 for Target Size (Level AAA)
+    minHeight: 96, // Set minimum width of each item to 96 for Target Size (Level AAA)
+  },
 });
