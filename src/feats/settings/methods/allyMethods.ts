7c325d4d8654128f16468c1e7f29cdd7213f815d
Max Weinmann <mweinmann@stud.hs-bremen.de>
2024-01-23 11:38:20 +0100

    cleans

--- src/feats/settings/methods/allyMethods.ts
+++ src/feats/settings/methods/allyMethods.ts
@@ -22,7 +22,7 @@ export interface AllyMethodParameters {
 // For the visual presentation of blocks of text, a mechanism is available to achieve the following:
 // (Foreground and background colors can be selected by the user.)
 // X not implemented! (Width is no more than 80 characters or glyphs (40 if CJK).)
-//Text is not justified (aligned to both the left and the right margins) V .
+// Text is not justified (aligned to both the left and the right margins) V .
 // Line spacing (leading) is at least space-and-a-half within paragraphs, and paragraph spacing is at least 1.5 times larger than the line spacing.
 // Text can be resized without assistive technology up to 200 percent in a way that does not require the user to scroll horizontally to read a line of text on a full-screen window V.
 
